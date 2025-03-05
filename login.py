a4a5db9a449f383e4acdff9aba265f7ac8fefb35
Babak Seyfikar <Snipguy.business@gmail.com>
2025-03-05 13:48:10 +0330

    Sent the first message to my user , now I need to make the necessary features available through the selenium

--- login.py
+++ login.py
@@ -1,38 +1,64 @@
-from selenium import webdriver
+from selenium import webdriver # type: ignore
 import pickle
 from time import time, sleep
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By # type: ignore
+from selenium.webdriver.support.ui import WebDriverWait # type: ignore
+from selenium.webdriver.support import expected_conditions as EC # type: ignore
+from selenium.common.exceptions import NoSuchElementException # type: ignore
 
 
 def main():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument(r"user-data-dir=C:\Selenium")
+    options.add_argument("profile-directory=Default")
+    # options.add_argument("--remote-debugging-port=9222") # If you get an error like "Profile in use", try uncommenting this line
 
+    driver = webdriver.Chrome(options=options)
 
     driver.get('https://web.eitaa.com')
-    driver.execute_script("return document.readyState") == "complete"
-    driver.find_element(By.XPATH, '//*[@id="auth-pages"]/div/div[2]/div[1]/div/div[3]/div[2]/div[1]').send_keys("9944714198")
+    holder = False
 
-    driver.find_element(By.XPATH, '//*[@id="auth-pages"]/div/div[2]/div[1]/div/div[3]/button/div').click()
-
-
-    verification_code = input("Enter the 5-digit verification code sent to your phone: ")
-
-    while len(verification_code) != 5 or not verification_code.isdigit():
-        print("Invalid code. Please enter a 5-digit number.")
+    try:
+        number_field = WebDriverWait(driver, 20).until(lambda d: d.find_element(By.XPATH, '//*[@id="auth-pages"]/div/div[2]/div[1]/div/div[3]/div[2]/div[1]'))
+        user_number = input("Enter your phone number : (No +98 no 0 ,e.g. : 9114445555) :\n")
+        while len(user_number) != 10 or not user_number.isdigit():
+            print("Invalid number. Please enter the number correctly this time.")
+            user_number = input("Enter your phone number : (No +98 no 0 ,e.g. : 9114445555) :\n")
+        
+        number_field.send_keys(user_number)
+        driver.find_element(By.XPATH, '//*[@id="auth-pages"]/div/div[2]/div[1]/div/div[3]/button/div').click()
+    except NoSuchElementException:
         verification_code = input("Enter the 5-digit verification code sent to your phone: ")
 
-
-    verification_box = driver.find_element(By.XPATH, '//*[@id="auth-pages"]/div/div[2]/div[3]/div/div[3]/div/input')
-    verification_box.send_keys(verification_code)
-
-
-    sleep(20)
+        while len(verification_code) != 5 or not verification_code.isdigit():
+            print("Invalid code. Please enter a 5-digit number.")
+            verification_code = input("Enter the 5-digit verification code sent to your phone: ")
 
 
+        WebDriverWait(driver, 10).until(driver.find_element(By.XPATH, '//*[@id="auth-pages"]/div/div[2]/div[3]/div/div[3]/div/input').send_keys(verification_code))
+        holder = True
+    except Exception as e:
+        print(e)
+        return 1
 
-    # Save cookies to a file
-    with open('cookies.pkl', 'wb') as file:
-        pickle.dump(driver.get_cookies(), file)
+    if holder:
+        pass
+    else:
+        verification_code = input("Enter the 5-digit verification code sent to your phone: ")
+        while len(verification_code) != 5 or not verification_code.isdigit():
+            print("Invalid code. Please enter a 5-digit number.")
+            verification_code = input("Enter the 5-digit verification code sent to your phone: ")
+
+
+        WebDriverWait(driver, 10).until(driver.find_element(By.XPATH, '//*[@id="auth-pages"]/div/div[2]/div[3]/div/div[3]/div/input').send_keys(verification_code))
+
+    sleep(60)
+
+
+
+    # # Save cookies to a file
+    # with open('cookies.pkl', 'wb') as file:
+    #     pickle.dump(driver.get_cookies(), file)
 
     driver.quit()
 
--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 from time import sleep, time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium import webdriver # type: ignore
+from selenium.webdriver.common.by import By # type: ignore
 import pickle
 
 def verifyLogin():
-    time.sleep(10000)
+    sleep(10000)
 
 def main():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument(r"user-data-dir=C:\Selenium")
+    options.add_argument("profile-directory=Default")
+    # options.add_argument("--remote-debugging-port=9222") 
 
-    with open('cookies.pkl', 'rb') as file:
-        cookies = pickle.load(file)
-        for cookie in cookies:
-            driver.add_cookie(cookie)
+    driver = webdriver.Chrome(options=options)
 
-    driver.get('https://web.eitaa.com/')
 
-    time.sleep(10)
+    driver.execute_script("window.open('https://web.eitaa.com', '_blank');")  # Open new tab
+    driver.switch_to.window(driver.window_handles[1])  # Switch to the new tab
 
-    verifyLogin()
+
+    # with open('cookies.pkl', 'rb') as file:
+    #     cookies = pickle.load(file)
+    #     for cookie in cookies:
+    #         driver.add_cookie(cookie)
+    
+    # driver.refresh()
+    sleep(10)
+
+    # verifyLogin()
 
     # Locate the element with the specific data-peer-id
-    user_element = driver.find_element(By.CSS_SELECTOR ,'[data-peer-id="specific-peer-id"]')
-    user_element.click()
+    try:
+        user_element = driver.find_element(By.CSS_SELECTOR ,'[data-peer-id="19755659"]')
+        user_element.click()
+    except Exception as e:
+        print(e)
+        sleep(5000)
+
 
     # Send a message to the user
     message_box = driver.find_element(By.XPATH ,'//*[@id="column-center"]/div/div/div[4]/div/div[1]/div[7]/div[1]/div[1]')
